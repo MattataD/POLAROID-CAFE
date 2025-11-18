947f1cf4f983384613bf63d5f9bf8f2c07fdec39
Lord MIchael Sayson <kurokotetsuya748@gmail.com>
2025-11-18 17:29:11 +0800

    initial commit

---
+++ vidscript.js
@@ -0,0 +1,16 @@
+/* 🔥 VIDEO CAROUSEL */
+.video-carousel {
+    width: 100%;
+    height: 350px;
+    overflow: hidden;
+    white-space: nowrap;
+    display: flex;
+    scroll-behavior: smooth;
+    margin-top: 40px;
+}
+
+.carousel-video {
+    flex: 0 0 100%;
+    height: 100%;
+    object-fit: cover;
+}

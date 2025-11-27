c1ab6c77fa591be8b45f06ff1ee4b8f33640caef
r4keem <viggoku22@gmail.com>
2025-11-27 17:31:52 +0800

    Delete Contact Info Page Script.js

--- Contact Info Page Script.js
+++
@@ -1,7 +0,0 @@
-const headerEl = document.querySelector('.Nav-bar');
-headerEl.addEventListener('mouseenter', () => {
-    headerEl.classList.add('Nav-bar-hov');
-});
-headerEl.addEventListener('mouseleave', () => {
-    headerEl.classList.remove('Nav-bar-hov');
-});

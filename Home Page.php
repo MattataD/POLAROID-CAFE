cb91e652019068bcfc99f1a5edaeef9f8a6905f9
r4keem <viggoku22@gmail.com>
2025-12-10 21:28:39 +0800

    Add files via upload

--- Home Page.php
+++ Home Page.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Home Page Style.css">
     <title>Polaroid Cafe</title>
-    <link rel="icon" type="image/x-icon" href="../Images/Polaroid_Icon.svg">
+    <link rel="icon" type="image/x-icon" href="Images/Polaroid_Icon.svg">
 </head>
 <body>
     <img src="Images/A4-MENU.svg" alt="Background" class="background-img">
@@ -43,7 +43,14 @@
             <li class="nav-item-container">
                 <div class="link-items">
                     <img src="Images/Cart Fill Icon.svg" class="nav-icon" alt="">
-                    <a href="/Cart/Cart Page.php" class="link">Cart</a>
+                    <a href="Cart/Cart Page.php" class="link">Cart</a>
+                </div>
+            </li>
+
+            <li class="nav-item-container">
+                <div class="link-items">
+                    <img src="Images/Admin Fill Icon.svg" class="nav-icon" alt="">
+                    <a href="Admin Page.php" class="link">Admin</a>
                 </div>
             </li>
         </ul>
